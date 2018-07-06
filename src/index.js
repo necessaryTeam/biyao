66fb5c78730895c5c97d52f8b7012847f881a8b9
djm007djm <121291187@qq.com>
2018-07-06 17:56:34 +0800

    project start

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// 引入路由
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+
+
+
+// 引入页面
+
+
+
+
+
+
+
+
+
+
+//redux  Providershi是把store用在所有组件中
+import {Provider} from 'react-redux';
+// createStore 是创建仓库的方法
+import {createStore} from 'redux';
+
+// Store  创建仓库 类似Vuex的 store
+const store = createStore((state = {
+    count: 0,
+    isShowGallery:false,
+}, action) => {
+  const count = state.count
+  switch (action.type) {
+  	// 触发的动作（动作名字/触发的暗号）
+    case 'isxiejie':
+      return Object.assign({}, state, {
+      	isShowGallery: action.isShowGallery
+      });
+    case 'isfuquan':
+      return Object.assign({}, state, {
+        isShowGallery: action.isShowGallery
+      });
+    case 'isjiawen':
+      return Object.assign({}, state, {
+        idxShowMyNav: action.idxShowMyNav
+      });
+    case 'isjiaming':
+      return Object.assign({}, state, {
+        idxShowMyNav: action.idxShowMyNav
+      });
+    default:
+      return state
+  }
+});
+
+
+
+ReactDOM.render(
+<Router>
+  <Provider store={store}>
+  	<div>
+  		<App />
+
+  	</div>
+  </Provider>
+</Router>
+	
+	, document.getElementById('root'));
+registerServiceWorker();
